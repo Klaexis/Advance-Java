109865868832fc26acd4cc56fe9c4873417bb481
Aleck <alecklim1245@gmail.com>
2025-10-17 23:59:29 +0800

    Separated the Main file

--- Main.java
+++ Main.java
@@ -1,55 +1,66 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Main {
-//     public static void main(String[] args) {
-// 		AsciiTable app = new AsciiTable();
-		
-// 		Scanner sc = new Scanner(System.in);
-		
-// 		app.generateTable(sc);
-// 		app.print();
-		
-// 		boolean isRunning = true;
-// 		while(isRunning) {
-// 			System.out.print("1 - Search\n"+
-// 							 "2 - Edit\n"+
-// 							 "3 - Print\n"+
-// 							 "4 - Reset\n"+
-// 							 "x - Exit\n"+
-// 							 "Enter the function you want to do: ");
-							 
-// 			String choice = sc.next();
-// 			choice = choice.toLowerCase();
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AsciiTable app = new AsciiTable();
+        String fileName = AsciiTable.checkOrCreateFile(sc, args);
+        
+        app.setFileName(fileName);
+        app.loadFromFile(fileName);
+        app.printTable();
 
-// 			System.out.println();
+        boolean isRunning = true;
+        while (isRunning) {
+            System.out.print("[ search ] - Search\n" +
+                             "[ edit ] - Edit\n" +
+                             "[ add_row ] - Add Row\n" +
+                             "[ sort ] - Sort\n" +
+                             "[ print ] - Print\n" +
+                             "[ reset ] - Reset\n" +
+                             "[ x ] - Exit\n" +
+                             "Enter the function you want to do: ");
 
-// 			switch(choice) {
-// 				case "1": //Search
-// 					System.out.println("Searching...");
-// 					app.search(sc);
-// 					break;
-// 				case "2": //Edit
-// 					System.out.println("Editing...");
-// 					app.edit(sc);
-// 					break;
-// 				case "3": //Print
-// 					System.out.println("Printing...");
-// 					app.print();
-// 					break;
-// 				case "4": //Reset
-// 					System.out.println("Resetting...");
-// 					app.generateTable(sc);
-// 					app.print();
-// 					break;
-// 				case "x": //Exit
-// 					System.out.println("Exiting...");
-// 					isRunning = false;
-// 					sc.close();
-// 					System.exit(0); 
-// 					break;
-// 				default: //Invalid Input
-// 					System.out.println("Invalid Input\n");
-// 			}
-// 		}
-// 	}
-// }
+            String choice = sc.next().toLowerCase();
+            System.out.println();
+
+            switch (choice) {
+                case "search":
+                    System.out.println("Searching...");
+                    break;
+
+                case "edit":
+                    System.out.println("Editing...");
+                    break;
+
+                case "add_row":
+                    System.out.println("Adding Row...");
+                    break;
+
+                case "sort":
+                    System.out.println("Sorting...");
+                    break;
+
+                case "print":
+                    System.out.println("Printing...");
+                    app.printTable();
+                    break;
+
+                case "reset":
+                    System.out.println("Resetting...");
+                    app.resetTable(sc);
+                    break;
+
+                case "x":
+                    System.out.println("Exiting...");
+                    isRunning = false;
+                    sc.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid Input\n");
+            }
+        }
+    }
+}
