1c1623af08604acdd93e1340578456b2adb9d8d5
Aleck <alecklim1245@gmail.com>
2025-11-26 11:55:45 +0800

    Added comments

--- src/asciiproject/Main.java
+++ src/asciiproject/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import asciiproject.util.FileHandler;
 import asciiproject.service.TableService;
 
+//This is main
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
